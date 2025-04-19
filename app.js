d78f1559d6ca09b6e98860b191e1ff284719d2ce
manishsonak <manishsonak9@gmail.com>
2025-04-19 17:29:07 +0530

    update env file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.CORE_URL,
     methods:['POST','PUT','DELETE','GET'],
     credentials:true
 }))
